b04304c2e430ea33d9e933003e8b0eb445d1276e
Pierre Lanari <pierre.lanari@unil.ch>
2026-08-01 16:12:54 +0800

    Fixes for PR #55

--- Program/SourceControl/XMapTools_exported.m
+++ Program/SourceControl/XMapTools_exported.m
@@ -338,8 +338,8 @@ classdef XMapTools_exported < matlab.apps.AppBase
         Sampling_SelectStripeButton     matlab.ui.control.Button
         Sampling_ExportButton           matlab.ui.control.Button
         Sampling_ResetButton            matlab.ui.control.Button
-        Sampling_Plot1                  matlab.ui.control.UIAxes
         Sampling_Plot2                  matlab.ui.control.UIAxes
+        Sampling_Plot1                  matlab.ui.control.UIAxes
         StandardsTab                    matlab.ui.container.Tab
         GridLayout9_3                   matlab.ui.container.GridLayout
         SubTabStandard                  matlab.ui.container.TabGroup
@@ -363,8 +363,8 @@ classdef XMapTools_exported < matlab.apps.AppBase
         Std_Shift_Y                     matlab.ui.control.NumericEditField
         StdAll_Synchronize              matlab.ui.control.Button
         StdAll_profil                   matlab.ui.control.UIAxes
-        StdAll_map2                     matlab.ui.control.UIAxes
         StdAll_map1                     matlab.ui.control.UIAxes
+        StdAll_map2                     matlab.ui.control.UIAxes
         SpotDataTab                     matlab.ui.container.Tab
         GridLayout9_5                   matlab.ui.container.GridLayout
         SubTabSpotData                  matlab.ui.container.TabGroup
@@ -2847,7 +2847,8 @@ classdef XMapTools_exported < matlab.apps.AppBase
                             end
                             
                             if ~isfloat(app.Data2Plot)
-                                eval(['app.Data2Plot = app.Data2Plot .* ',class(app.Data2Plot),'(MaskMap);']);
+                                MaskMapLike = cast(MaskMap,'like',app.Data2Plot);
+                                app.Data2Plot = app.Data2Plot .* MaskMapLike;
                             else
                                 app.Data2Plot = app.Data2Plot.*MaskMap;
                             end
@@ -5350,7 +5351,7 @@ classdef XMapTools_exported < matlab.apps.AppBase
                 
                 if length(WhereMap) > 1             % We have several maps...
                     ElementSel = app.ElOxDataDef.ElList(app.XMapToolsData.MapData.It.ElInd(WhereMap(1)));
-                    uiconfirm(app.XMapTools_GUI,{['Several maps found for ',char(ElementSel)],app.XMapToolsData.MapData.It.Names{WhereMap},'The last map was arbitrarly selected!'},'XMapTools','Options', {'Contine'},'Icon', 'warning');
+                    uiconfirm(app.XMapTools_GUI,{['Several maps found for ',char(ElementSel)],app.XMapToolsData.MapData.It.Names{WhereMap},'The last map was arbitrarily selected!'},'XMapTools','Options', {'Continue'},'Icon', 'warning');
                     WhereMap = WhereMap(end);
                 end
                 
@@ -12201,7 +12202,7 @@ classdef XMapTools_exported < matlab.apps.AppBase
                     app.XMapToolsData.TrainingSet.MaskNode(Pos+i) = 0; 
                 end
                 app.XMapToolsData.TrainingSet.Nb(Pos+i) = TrainingSet.Nb(i);
-                app.XMapToolsData.TrainingSet.Data(Pos+i) = TrainingSet(i).Data;
+                app.XMapToolsData.TrainingSet.Data(Pos+i) = TrainingSet.Data(i);
             end
             
             app.WaitBar.Message = 'Updating the interface';
@@ -20307,19 +20308,19 @@ classdef XMapTools_exported < matlab.apps.AppBase
             app.Sampling_ResetButton.Layout.Column = 7;
             app.Sampling_ResetButton.Text = '';
 
-            % Create Sampling_Plot1
-            app.Sampling_Plot1 = uiaxes(app.GridLayout9_2);
-            app.Sampling_Plot1.PlotBoxAspectRatio = [1.02534562211982 1 1];
-            app.Sampling_Plot1.FontSize = 9;
-            app.Sampling_Plot1.Layout.Row = [3 10];
-            app.Sampling_Plot1.Layout.Column = [1 7];
-
             % Create Sampling_Plot2
             app.Sampling_Plot2 = uiaxes(app.GridLayout9_2);
             app.Sampling_Plot2.PlotBoxAspectRatio = [1.02534562211982 1 1];
             app.Sampling_Plot2.FontSize = 9;
             app.Sampling_Plot2.Layout.Row = [12 19];
             app.Sampling_Plot2.Layout.Column = [1 7];
+
+            % Create Sampling_Plot1
+            app.Sampling_Plot1 = uiaxes(app.GridLayout9_2);
+            app.Sampling_Plot1.PlotBoxAspectRatio = [1.02534562211982 1 1];
+            app.Sampling_Plot1.FontSize = 9;
+            app.Sampling_Plot1.Layout.Row = [3 10];
+            app.Sampling_Plot1.Layout.Column = [1 7];
 
             % Create StandardsTab
             app.StandardsTab = uitab(app.TabGroup);
@@ -20501,16 +20502,6 @@ classdef XMapTools_exported < matlab.apps.AppBase
             app.StdAll_profil.Layout.Row = [1 3];
             app.StdAll_profil.Layout.Column = [1 2];
 
-            % Create StdAll_map2
-            app.StdAll_map2 = uiaxes(app.GridLayout11);
-            title(app.StdAll_map2, 'sqrt(sum(corrcoef^2))')
-            app.StdAll_map2.Toolbar.Visible = 'off';
-            app.StdAll_map2.PlotBoxAspectRatio = [1.39236111111111 1 1];
-            app.StdAll_map2.FontSize = 9;
-            app.StdAll_map2.Box = 'on';
-            app.StdAll_map2.Layout.Row = [9 12];
-            app.StdAll_map2.Layout.Column = [1 2];
-
             % Create StdAll_map1
             app.StdAll_map1 = uiaxes(app.GridLayout11);
             title(app.StdAll_map1, 'Element')
@@ -20520,6 +20511,16 @@ classdef XMapTools_exported < matlab.apps.AppBase
             app.StdAll_map1.Box = 'on';
             app.StdAll_map1.Layout.Row = [5 8];
             app.StdAll_map1.Layout.Column = [1 2];
+
+            % Create StdAll_map2
+            app.StdAll_map2 = uiaxes(app.GridLayout11);
+            title(app.StdAll_map2, 'sqrt(sum(corrcoef^2))')
+            app.StdAll_map2.Toolbar.Visible = 'off';
+            app.StdAll_map2.PlotBoxAspectRatio = [1.39236111111111 1 1];
+            app.StdAll_map2.FontSize = 9;
+            app.StdAll_map2.Box = 'on';
+            app.StdAll_map2.Layout.Row = [9 12];
+            app.StdAll_map2.Layout.Column = [1 2];
 
             % Create SpotDataTab
             app.SpotDataTab = uitab(app.TabGroup);
